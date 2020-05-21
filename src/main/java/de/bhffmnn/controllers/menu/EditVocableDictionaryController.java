a7f7fd7d510ca4f42eabdc9156c9bae9e0b93f6e
Benjamin Hoffmann <44234556+bhffmnn@users.noreply.github.com>
2020-05-21 13:31:30 +0200

    Add filter to EditVocableDictionary

--- src/main/java/de/bhffmnn/controllers/menu/EditVocableDictionaryController.java
+++ src/main/java/de/bhffmnn/controllers/menu/EditVocableDictionaryController.java
@@ -20,20 +20,47 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class EditVocableDictionaryController implements Initializable {
 
     @FXML
     TableView dictTable;
+
+    //Filter stuff
+    @FXML
+    TextField formFilterField;
+    @FXML
+    TextField readFilterField;
+    @FXML
+    TextField meanFilterField;
+
+    @FXML
+    private Spinner<Integer> levelSpinner;
+
+    @FXML
+    DatePicker duePicker;
+
+    @FXML
+    CheckBox formCheckBox;
+    @FXML
+    CheckBox readCheckBox;
+    @FXML
+    CheckBox meanCheckBox;
+    @FXML
+    CheckBox levelCheckBox;
+    @FXML
+    CheckBox dueCheckBox;
+
+    private VocableFilter vocableFilter;
 
     private TableColumn<String, Vocable> formClmn;
     private TableColumn<String, Vocable> readClmn;
@@ -48,6 +75,7 @@ public class EditVocableDictionaryController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         cloneDictionary = App.vocableDictionary.cloneDeep();
+        vocableFilter = new VocableFilter();
 
         formClmn = createColumn("Form", "form");
         readClmn = createColumn("Reading", "reading");
@@ -67,6 +95,23 @@ public class EditVocableDictionaryController implements Initializable {
             dictTable.getItems().add(v);
         }
 
+        //Filter part
+        formFilterField.setDisable(true);
+        readFilterField.setDisable(true);
+        meanFilterField.setDisable(true);
+
+        levelSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10));
+        levelSpinner.getValueFactory().setValue(0);
+        levelSpinner.setDisable(true);
+
+        duePicker.setValue(LocalDate.now());
+        duePicker.setDisable(true);
+
+        setUpCheckBox(formCheckBox, formFilterField);
+        setUpCheckBox(readCheckBox, readFilterField);
+        setUpCheckBox(meanCheckBox, meanFilterField);
+        setUpCheckBox(levelCheckBox, levelSpinner);
+        setUpCheckBox(dueCheckBox, duePicker);
     }
 
 
@@ -136,6 +181,41 @@ public class EditVocableDictionaryController implements Initializable {
         reloadTable();
     }
 
+    @FXML
+    private void applyButtonAction(ActionEvent actionEvent) throws Exception {
+        if (formFilterField.isDisabled()) {
+            vocableFilter.setFormFilter(null);
+        } else {
+            vocableFilter.setFormFilter(formFilterField.getText());
+        }
+
+        if (readFilterField.isDisabled()) {
+            vocableFilter.setReadFilter(null);
+        } else {
+            vocableFilter.setReadFilter(readFilterField.getText());
+        }
+
+        if (meanFilterField.isDisabled()) {
+            vocableFilter.setMeanFilter(null);
+        } else {
+            vocableFilter.setMeanFilter(meanFilterField.getText());
+        }
+
+        if (levelSpinner.isDisabled()) {
+            vocableFilter.setLevelFilter(null);
+        } else {
+            vocableFilter.setLevelFilter(levelSpinner.getValue());
+        }
+
+        if (duePicker.isDisabled()) {
+            vocableFilter.setDueFilter(null);
+        } else {
+            vocableFilter.setDueFilter(duePicker.getValue());
+        }
+
+        reloadTable();
+    }
+
     private TableColumn<String, Vocable> createColumn(String title, String feature) {
         TableColumn<String, Vocable> newColumn = new TableColumn<>(title);
         newColumn.setCellValueFactory(new PropertyValueFactory<>(feature));
@@ -144,8 +224,111 @@ public class EditVocableDictionaryController implements Initializable {
 
     private void reloadTable() {
         dictTable.getItems().clear();
-        for (Vocable v : cloneDictionary) {
+
+        VocableDictionary filteredDictionary = vocableFilter.filter(cloneDictionary);
+
+        for (Vocable v : filteredDictionary) {
             dictTable.getItems().add(v);
         }
     }
+
+    private void setUpCheckBox(CheckBox checkBox, Node node) {
+        checkBox.selectedProperty().addListener(((observableValue, oldValue, newValue) -> {
+            if (newValue) {
+                node.setDisable(false);
+            }
+            else {
+                node.setDisable(true);
+            }
+        }));
+    }
+
+    private class VocableFilter {
+        private String formFilter;
+        private String readFilter;
+        private String meanFilter;
+        private Integer levelFilter;
+        private LocalDate dueFilter;
+
+        private VocableFilter() {
+            formFilter = readFilter = meanFilter = null;
+            levelFilter = null;
+            dueFilter = null;
+        }
+
+        public String getFormFilter() {
+            return formFilter;
+        }
+        public String getMeanFilter() {
+            return meanFilter;
+        }
+        public String getReadFilter() {
+            return readFilter;
+        }
+        public Integer getLevelFilter() {
+            return levelFilter;
+        }
+        public LocalDate getDueFilter() {
+            return dueFilter;
+        }
+
+        public void setFormFilter(String formFilter) {
+            this.formFilter = formFilter;
+        }
+        public void setMeanFilter(String meanFilter) {
+            this.meanFilter = meanFilter;
+        }
+        public void setReadFilter(String readFilter) {
+            this.readFilter = readFilter;
+        }
+        public void setLevelFilter(Integer levelFilter) {
+            this.levelFilter = levelFilter;
+        }
+        public void setDueFilter(LocalDate dueFilter) {
+            this.dueFilter = dueFilter;
+        }
+
+        public VocableDictionary filter(VocableDictionary vocableDictionary) {
+            VocableDictionary filteredDictionary = vocableDictionary.clone();
+            if (!(formFilter == null)) {
+                filteredDictionary = new VocableDictionary();
+                filteredDictionary.add(vocableDictionary.getByForm(formFilter));
+            }
+            if (!(readFilter == null)) {
+                ArrayList<Vocable> removalList = new ArrayList<>();
+                for (Vocable v : filteredDictionary) {
+                    if (!v.getReading().contains(readFilter)) {
+                        removalList.add(v);
+                    }
+                }
+                filteredDictionary.removeAll(removalList);
+            }
+            if (!(meanFilter == null)) {
+                ArrayList<Vocable> removalList = new ArrayList<>();
+                for (Vocable v : filteredDictionary) {
+                    if (!v.getMeaning().contains(meanFilter)) {
+                        removalList.add(v);
+                    }
+                }
+                filteredDictionary.removeAll(removalList);
+            }
+            if (!(levelFilter == null)) {
+                VocableDictionary removalList = filteredDictionary.clone();
+                removalList.removeAll(filteredDictionary.getByLevel(levelFilter));
+                filteredDictionary.removeAll(removalList);
+            }
+            if (!(dueFilter == null)) {
+                ArrayList<Vocable> removalList = new ArrayList<>();
+                for (Vocable v : filteredDictionary) {
+                    if (v.getDue().isAfter(dueFilter.plusDays(1))) {
+                        removalList.add(v);
+                    }
+                }
+                filteredDictionary.removeAll(removalList);
+            }
+
+            return filteredDictionary;
+        }
+    }
+
 }
